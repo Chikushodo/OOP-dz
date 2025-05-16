a954c13ac6e1a304305e057340c4800d86bf0f48
Chikushodo <149254988+Chikushodo@users.noreply.github.com>
2025-05-16 15:41:23 +0700

    Add files via upload

---
+++ 7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pain;
+
+public static class StringExtension
+{
+    public static string FullnameAndGrade(this Student student)
+    {
+        return $" {student.Name}, {student.Surname}, успеваемость: {student.Grade}";
+    }
+}
+public class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public int Grade { get; set; }
+
+    public Student(string name, string surname, int grade)
+    {
+        Name = name;
+        Surname = surname;
+        Grade = grade;
+    }
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student("Эдвард", "Каллен" , 5);
+        string result = student.FullnameAndGrade();
+        Console.WriteLine(result);
+    }
+}
