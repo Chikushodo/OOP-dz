b28f22be8045237fffa1033bad06c5a63a9db056
Chikushodo <149254988+Chikushodo@users.noreply.github.com>
2025-05-29 09:37:07 +0700

    Add files via upload

---
+++ uuu.cs
@@ -0,0 +1,71 @@
+﻿public interface IWeapon
+{
+    void Use();
+}
+
+public interface IRangedWeapon : IWeapon
+{
+    void Reload();
+}
+
+public interface IMeleeWeapon : IWeapon
+{
+    void Swing();
+}
+
+public class Bow : IRangedWeapon
+{
+    public void Use()
+    {
+        Console.WriteLine("Стреляю из лука!");
+    }
+
+    public void Reload()
+    {
+        Console.WriteLine("Перезарядка лука.");
+    }
+}
+
+public class Sword : IMeleeWeapon
+{
+    public void Use()
+    {
+        Console.WriteLine("Атакую мечом!");
+    }
+
+    public void Swing()
+    {
+        Console.WriteLine("Размахиваю мечом.");
+    }
+}
+
+public class Crossbow : IRangedWeapon
+{
+    public void Use()
+    {
+        Console.WriteLine("Стреляю из арбалета!");
+    }
+
+    public void Reload()
+    {
+        Console.WriteLine("Перезарядка арбалета.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IRangedWeapon bow = new Bow();
+        bow.Use();
+        bow.Reload();
+
+        IMeleeWeapon sword = new Sword();
+        sword.Use();
+        sword.Swing();
+
+        IRangedWeapon crossbow = new Crossbow();
+        crossbow.Use();
+        crossbow.Reload();
+    }
+}
